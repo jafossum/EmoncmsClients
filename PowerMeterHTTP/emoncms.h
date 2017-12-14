15b35f6b907a4635577a2e0559530b0a8c3456c5
John A. Fossum <john.fossum@q-free.com>
2017-12-14 09:22:19 +0100

    Indent

--- PowerMeterHTTP/emoncms.h
+++ PowerMeterHTTP/emoncms.h
@@ -7,11 +7,11 @@
 class EmonCms
 {
 private:
-     //Emoncms configurations
-     const char* server = "emoncms.org";     // name address for emoncms.org
-     const int httpsPort = 443;
-     const int node = 0;                         //if 0, not used
-     // SHA1 fingerprint of the certificate
+    //Emoncms configurations
+    const char* server = "emoncms.org";     // name address for emoncms.org
+    const int httpsPort = 443;
+    const int node = 0;                         //if 0, not used
+    // SHA1 fingerprint of the certificate
     const char* fingerprint = "B4 58 91 74 C9 33 52 18 1A A5 A1 81 32 60 9D CB 6E 69 53 C2";
     // HTTPS Client
     WiFiClientSecure secureClient;
